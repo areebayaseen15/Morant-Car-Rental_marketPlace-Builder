c4b6cf70f2a2208fcd7cd4a72fec6174a4d30717
Areeba Yaseen <147875287+areebayaseen15@users.noreply.github.com>
2025-02-11 17:45:51 +0500

    Update Profile.tsx

--- Morant_car_rental_MarketPlace-Builder/src/app/Components/Profile.tsx
+++ Morant_car_rental_MarketPlace-Builder/src/app/Components/Profile.tsx
@@ -1,32 +1,33 @@
-"use client"
+"use client";
 
-import React from 'react'
+import React from "react";
 import {
-    SignInButton,
-    SignedIn,
-    SignedOut,
-    UserButton,
-  } from '@clerk/nextjs'
-  
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
 const Profile = () => {
   return (
-    <div>
-        <div className=" rounded-full cursor-pointer mt-2 w-12 h-12 border-[1px] border-[#C3D4E9] border-opacity-[80%] px-[7px] py-[7px]">
-          <SignedOut >
-           
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-
-           
-
-            <UserButton />
-
-          </SignedIn>
-          </div>
-
+    <div >
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton
+            appearance={{
+              elements: {
+                avatarBox: "w-12 h-12 relative right-2 bottom-1",
+                userButtonBox:"w-10 h-12", 
+              },
+            }}
+          />
+        </SignedIn>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
