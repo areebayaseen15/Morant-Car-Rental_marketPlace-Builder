73be9def7163200d6260fae41394c601371de565
Areeba Yaseen <147875287+areebayaseen15@users.noreply.github.com>
2025-02-12 03:36:19 +0500

    Update DarkMode.tsx

--- Morant_car_rental_MarketPlace-Builder/src/app/Components/DarkMode.tsx
+++ Morant_car_rental_MarketPlace-Builder/src/app/Components/DarkMode.tsx
@@ -9,7 +9,8 @@ const DarkModeToggle = () => {
       onClick={toggleTheme}
       className="p-2 rounded bg-gray-200 dark:bg-gray-800 text-black dark:text-white"
     >
-      {theme === "dark" ? "🌙 Dark Mode" : "☀️ Light Mode"}
+      {theme === "dark" ? "🌙 Dark Mode" : "☀️ Light Mode"}  
+      
     </button>
   );
 };
