b9f820bb876ee52cb4a175a54c3df6c148ac0141
Areeba Yaseen <147875287+areebayaseen15@users.noreply.github.com>
2025-02-17 01:32:06 +0500

    Update page.tsx

--- Morant_car_rental_MarketPlace-Builder/src/app/Cars/[slug]/Billing/page.tsx
+++ Morant_car_rental_MarketPlace-Builder/src/app/Cars/[slug]/Billing/page.tsx
@@ -3,14 +3,13 @@
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutPage from "@/app/Components/CheckoutPage";
-import { useEffect, useState , use} from "react";
+import { useEffect, useState, use } from "react";
 import Image from "next/image";
 import { AiFillStar } from "react-icons/ai";
 import { urlFor } from "@/sanity/lib/image";
 import { client } from "@/sanity/lib/client";
 import Navbar from "@/app/Components/navbar";
 import Link from "next/link";
-
 
 interface Car {
   name: string;
@@ -25,7 +24,7 @@ interface Car {
 }
 
 interface PageProps {
-  params: Promise<{ slug: string }>; 
+  params: Promise<{ slug: string }>;
 }
 
 if (!process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY) {
@@ -69,12 +68,12 @@ const Payment = ({ params }: PageProps) => {
   function convertToSubcurrency(amount: number, factor = 100) {
     return Math.round(amount * factor);
   }
-  
+
   const pricePerDay = cleanPrice(car?.pricePerDay || "0");
   const amount = convertToSubcurrency(pricePerDay);
 
   return (
-    <div>
+    <div className="bg-[#F6F7F9] dark:bg-gray-900 text-black dark:text-white">
       <Navbar />
       <div className="mt-32 w-full h-auto max-w-[1440px] flex flex-col-reverse lg:flex-row gap-5 space-y-8">
         <Elements
@@ -88,94 +87,93 @@ const Payment = ({ params }: PageProps) => {
           <CheckoutPage amount={pricePerDay} />
         </Elements>
         {/* Rental Summary */}
-        <div className="mx-10 max-w-[700px] lg:h-[560px] rounded-lg bg-white space-y-8 p-10 xl:mr-10">
-        <div className="mb-4 lg:mb-6">
-          <h2 className="text-[18px] sm:text-[20px] font-bold text-[#1A202C]">
-            Rental Summary
-          </h2>
-          <p className="text-[12px] sm:text-[14px] text-[#90A3BF] font-medium">
-            Prices may change depending on the length of the rental and the
-            price of your rental car.
-          </p>
-        </div>
-        <div className="flex flex-col sm:flex-row items-center gap-4 mb-4">
-          <div className="w-[96px] h-[72px] bg-[#5b74c7] rounded-lg flex justify-center items-center">
-            {car?.image ? (
-              <Image
-                src={urlFor(car.image).url()}
-                alt={car?.name || "Car"}
-                width={116}
-                height={36}
-                className="rounded-md object-cover"
-              />
-            ) : (
-              <p>No Image Available</p>
-            )}
+        <div className="mx-10 max-w-[700px] lg:h-[560px] rounded-lg bg-white dark:bg-slate-800 space-y-8 p-10 xl:mr-10">
+          <div className="mb-4 lg:mb-6">
+            <h2 className="text-[18px] sm:text-[20px] font-bold text-[#1A202C] dark:text-white">
+              Rental Summary
+            </h2>
+            <p className="text-[12px] sm:text-[14px] text-[#90A3BF] font-medium">
+              Prices may change depending on the length of the rental and the
+              price of your rental car.
+            </p>
           </div>
-          <div className="text-center sm:text-left">
-            <h3 className="text-[20px] sm:text-[28px] font-bold text-[#1A202C]">
-              {car?.name || "Unknown Car"}
-            </h3>
-            <div className="flex items-center justify-center sm:justify-start text-[#FFCC00] text-[14px] sm:text-[16px]">
-              <AiFillStar />
-              <AiFillStar />
-              <AiFillStar />
-              <AiFillStar />
-              <span className="text-[#90A3BF] text-[12px] sm:text-[14px] ml-2">
-                440+ Reviews
+          <div className="flex flex-col sm:flex-row items-center gap-4 mb-4">
+            <div className="w-[96px] h-[72px] bg-[#5b74c7] rounded-lg flex justify-center items-center">
+              {car?.image ? (
+                <Image
+                  src={urlFor(car.image).url()}
+                  alt={car?.name || "Car"}
+                  width={116}
+                  height={36}
+                  className="rounded-md object-cover"
+                />
+              ) : (
+                <p>No Image Available</p>
+              )}
+            </div>
+            <div className="text-center sm:text-left">
+              <h3 className="text-[20px] sm:text-[28px] font-bold text-[#1A202C] dark:text-white">
+                {car?.name || "Unknown Car"}
+              </h3>
+              <div className="flex items-center justify-center sm:justify-start text-[#FFCC00] text-[14px] sm:text-[16px]">
+                <AiFillStar />
+                <AiFillStar />
+                <AiFillStar />
+                <AiFillStar />
+                <span className="text-[#90A3BF] text-[12px] sm:text-[14px] ml-2">
+                  440+ Reviews
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="mb-4 lg:mb-6">
+            <div className="flex justify-between items-center mb-2">
+              <span className="text-[14px] sm:text-[16px] font-medium text-[#90A3BF]">
+                Subtotal
+              </span>
+              <span className="text-[14px] sm:text-[16px] font-semibold text-[#1A202C] dark:text-white mr-10">
+                ${pricePerDay}
+              </span>
+            </div>
+            <div className="flex justify-between items-center">
+              <span className="text-[14px] sm:text-[16px] font-medium text-[#90A3BF]">
+                Tax
+              </span>
+              <span className="text-[14px] sm:text-[16px] font-semibold text-[#1A202C] dark:text-white mr-10">
+                $0
               </span>
             </div>
           </div>
-        </div>
-        <div className="mb-4 lg:mb-6">
-          <div className="flex justify-between items-center mb-2">
-            <span className="text-[14px] sm:text-[16px] font-medium text-[#90A3BF]">
-              Subtotal
-            </span>
-            <span className="text-[14px] sm:text-[16px] font-semibold text-[#1A202C] mr-10">
-              ${pricePerDay}
-            </span>
-          </div>
-          <div className="flex justify-between items-center">
-            <span className="text-[14px] sm:text-[16px] font-medium text-[#90A3BF]">
-              Tax
-            </span>
-            <span className="text-[14px] sm:text-[16px] font-semibold text-[#1A202C] mr-10">
-              $0
-            </span>
-          </div>
-        </div>
 
-        {/* Promo Code */}
-        <div className="mb-4 lg:mb-6 flex flex-col sm:flex-row items-center gap-4 lg:gap-2">
-          <input
-            type="text"
-            placeholder="Apply promo code"
-            className="flex-grow h-[40px] sm:h-[48px] bg-[#F6F7F9] rounded-lg px-4 text-[12px] sm:text-[14px] placeholder-[#90A3BF] focus:outline-none"
-          />
-          <Link href="/Dashboard">
-            <button className="w-full sm:w-[92px] h-[40px] sm:h-[48px] text-black text-[14px] sm:text-[16px] font-semibold rounded-lg">
-              Apply now
-            </button>
-          </Link>
-        </div>
-
-        {/* Total Price */}
-        <div className="border-t border-[#EDEDED] pt-4">
-          <div className="flex flex-col sm:flex-row justify-between items-center mb-2">
-            <span className="text-[16px] sm:text-[20px] font-bold text-[#1A202C]">
-              Total Rental Price
-            </span>
-            <span className="text-[20px] sm:text-[24px] font-bold text-[#3563E9]">
-              ${pricePerDay}
-            </span>
+          {/* Promo Code */}
+          <div className="mb-4 lg:mb-6 flex flex-col sm:flex-row items-center gap-4 lg:gap-2">
+            <input
+              type="text"
+              placeholder="Apply promo code"
+              className="flex-grow h-[40px] sm:h-[48px] bg-[#F6F7F9] dark:bg-gray-700 rounded-lg px-4 text-[12px] sm:text-[14px] placeholder-[#90A3BF] focus:outline-none"
+            />
+            <Link href="/Dashboard">
+              <button className="w-full sm:w-[92px] h-[40px] sm:h-[48px] text-black dark:text-white text-[14px] sm:text-[16px] font-semibold rounded-lg">
+                Apply now
+              </button>
+            </Link>
           </div>
-          <p className="text-[12px] sm:text-[14px] text-[#90A3BF] text-center sm:text-left">
-            Overall price and includes rental discount
-          </p>
+
+          {/* Total Price */}
+          <div className="border-t border-[#EDEDED] dark:border-gray-600 pt-4">
+            <div className="flex flex-col sm:flex-row justify-between items-center mb-2">
+              <span className="text-[16px] sm:text-[20px] font-bold text-[#1A202C] dark:text-white">
+                Total Rental Price
+              </span>
+              <span className="text-[20px] sm:text-[24px] font-bold text-[#3563E9]">
+                ${pricePerDay}
+              </span>
+            </div>
+            <p className="text-[12px] sm:text-[14px] text-[#90A3BF] text-center sm:text-left">
+              Overall price and includes rental discount
+            </p>
+          </div>
         </div>
-      </div>
-    
       </div>
     </div>
   );
