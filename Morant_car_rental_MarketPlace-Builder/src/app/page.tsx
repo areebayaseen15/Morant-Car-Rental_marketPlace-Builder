32ae2aa425020372c420184f0a8a5c631e4523b8
Areeba Yaseen <147875287+areebayaseen15@users.noreply.github.com>
2025-02-16 23:45:35 +0500

    Update page.tsx

--- Morant_car_rental_MarketPlace-Builder/src/app/page.tsx
+++ Morant_car_rental_MarketPlace-Builder/src/app/page.tsx
@@ -5,12 +5,10 @@ import Navbar from "./Components/navbar";
 const Home = () => {
   return (
     <div>
-      <div className="mt-36 lg:mt-40 bg-[#F6F7F9]  ">
       <Navbar/>
 
 
         <Hero />
-      </div>
     </div>
   );
 };
