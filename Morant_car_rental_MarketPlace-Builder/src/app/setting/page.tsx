cce2f7d0a70128d96e6b4fe6faa31c3bfe23fa59
Areeba Yaseen <147875287+areebayaseen15@users.noreply.github.com>
2025-02-18 01:54:29 +0500

    Update page.tsx

--- Morant_car_rental_MarketPlace-Builder/src/app/setting/page.tsx
+++ Morant_car_rental_MarketPlace-Builder/src/app/setting/page.tsx
@@ -7,7 +7,7 @@ const SettingsPage = () => {
   return (
     <div className="bg-white dark:bg-gray-900 text-black dark:text-white min-h-screen">
       <Navbar />
-      <div className="container mx-auto py-10 px-6 max-w-4xl">
+      <div className="container mx-auto py-10 px-6 max-w-4xl mt-40">
         <h1 className="text-4xl font-extrabold mb-10 text-center text-blue-600 dark:text-blue-400">
           Account Settings
         </h1>
