4f53d417cb70acf3e8121dd3df4206c934f8e30c
Areeba Yaseen <147875287+areebayaseen15@users.noreply.github.com>
2025-02-18 19:17:14 +0500

    Update CheckoutPage.tsx

--- Morant_car_rental_MarketPlace-Builder/src/app/Components/CheckoutPage.tsx
+++ Morant_car_rental_MarketPlace-Builder/src/app/Components/CheckoutPage.tsx
@@ -207,7 +207,7 @@ const CheckoutPage = ({ amount }: { amount: number }) => {
                   </select>
                 </div>
 
-                {/* Date Selection */}
+{/*                 Date Selection */}
                 <div>
                   <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">Date</label>
                   <input
