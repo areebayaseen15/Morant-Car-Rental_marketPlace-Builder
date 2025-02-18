b7e0cb5b07aefe268b8ef5918eb6672edea63cb3
Areeba Yaseen <147875287+areebayaseen15@users.noreply.github.com>
2025-02-18 19:19:38 +0500

    Update FetchCars.tsx

--- Morant_car_rental_MarketPlace-Builder/src/app/Components/FetchCars.tsx
+++ Morant_car_rental_MarketPlace-Builder/src/app/Components/FetchCars.tsx
@@ -2,24 +2,38 @@ import { client } from "@/sanity/lib/client";
 
 export interface ProductData {
   tags: string[];
+  oldPrice?: string;
+  newPrice?: string;
+  model?: string;
+  pricePerDay?: string;
   name?: string;
-  type: string;
+  seatingCapacity?: string;
+  fuelCapacity?: string;
+  transmission?: string;
+  title?: string;
   image?: {
     _type: string;
     asset: {
       _ref: string;
       _type: string;
-      current: string;
+      current?: string;
     };
   };
-  slug: {
-    current: string;
+  type?: string;
+  quantity?: number;
+  slug?: {
+    current?: string;
   };
+  description?: string;
+  brand?: string;
+  _type?: "product";
+  _id?: string;
+  
 }
 
 export const fetchCars = async (): Promise<ProductData[]> => {
   try {
-    const query = `*[_type == "car"]{name, tags, type, slug, image}`;
+    const query = `*[_type == "car"]`;
     const data = await client.fetch(query);
     return data;
   } catch (error) {
