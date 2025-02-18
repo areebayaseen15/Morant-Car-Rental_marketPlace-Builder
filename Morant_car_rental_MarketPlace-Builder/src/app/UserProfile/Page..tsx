553d0e383a64f6301db590f03f38585566c5669f
Areeba Yaseen <147875287+areebayaseen15@users.noreply.github.com>
2025-02-19 02:22:09 +0500

    Update Page..tsx

--- Morant_car_rental_MarketPlace-Builder/src/app/UserProfile/Page..tsx
+++ Morant_car_rental_MarketPlace-Builder/src/app/UserProfile/Page..tsx
@@ -1,18 +1,45 @@
 "use client";
 
-import { useUser } from "@clerk/nextjs";
+import React from "react";
+import { useRouter } from "next/navigation";
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
-const Dashboard = () => {
-  const { user } = useUser();
+const Profile = () => {
+  const router = useRouter();
+
+  const handleProfileClick = () => {
+    router.push("/AnalyticDashboard"); // Redirect to dashboard
+  };
 
   return (
-    <div className="container mx-auto mt-8">
-      <h1 className="text-2xl font-bold">Welcome to Your Dashboard</h1>
-      <p className="mt-4">Hello, {user?.firstName || "User"}!</p>
-      <p>Email: {user?.emailAddresses[0]?.emailAddress}</p>
-     
+    <div className="dark:bg-gray-900 dark:text-white">
+      <div>
+        {/* Show Sign In Button when user is signed out */}
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+
+        {/* Custom Clickable Profile */}
+        <SignedIn>
+          <div className="cursor-pointer" onClick={handleProfileClick}>
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-12 h-12 relative right-2 bottom-1",
+                  userButtonBox: "w-10 h-11 relative top-2 left-1",
+                },
+              }}
+            />
+          </div>
+        </SignedIn>
+      </div>
     </div>
   );
 };
 
-export default Dashboard;
+export default Profile;
